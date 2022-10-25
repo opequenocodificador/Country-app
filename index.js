b302fcfa80ed608a67f760f0b057fd3d7492c198
Monciaux Sylvain <monciauxsylvain63@gmail.com>
2022-10-25 09:26:43 +0100

    affichage v1 ajout de la fonction filter rajouter slice

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP OK
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-//coutry.name.includes(inputSearch.value);
+//coutry.name.includes(inputSearch.value); OK
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
@@ -24,17 +24,27 @@ async function fetchCountries() {
   await fetch(`https://restcountries.com/v3.1/all`)
     .then((res) => res.json())
     .then((data) => (countriesData = data));
-  console.log(countriesData);
+  //console.log(countriesData);
   countriesDisplay();
 }
 
 //Fonction affichage
 
 function countriesDisplay() {
-  countriesContainer.innerHTML = countriesData
-    .map(
-      (country) =>
-        `
+  if (countriesData === null) {
+    countriesContainer.innerHTML = `<div class="card">
+    <h2>Pas de résultat</h2>
+    </div>
+    `;
+  } else {
+    countriesContainer.innerHTML = countriesData
+      .filter((country) =>
+        country.translations.fra.common.includes(input.value)
+      )
+
+      .map(
+        (country) =>
+          `
   <div class="card">
   <img src=${country.flags.svg}>
   <h3>${country.translations.fra.common}</h3>
@@ -42,8 +52,9 @@ function countriesDisplay() {
   <h3>Population: ${country.population}
   </div>
   `
-    )
-    .join("");
+      )
+      .join("");
+  }
 }
 
 //Addevent
@@ -54,5 +65,4 @@ window.addEventListener("load", fetchCountries);
 //récupération des données de l'input pour le filter
 input.addEventListener("input", (e) => {
   fetchCountries(e.target.value);
-  console.log(e.target.value);
 });
